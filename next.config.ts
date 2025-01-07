6d9a4154c73789a11a5983c8ecafb7e70ac4a3c3
junsobi <junsobi@gmail.com>
2025-01-07 12:59:36 +0900

    chor : next/image로 외부이미지를 처리가능하게 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**", // 모든 경로 허용
+      },
+    ],
+  },
 };
 
 export default nextConfig;
